25be5372f090f800716ae58865a4c5f8a299a58f
carlacodes <carla.griffiths@gmail.com>
2022-07-26 13:56:31 +0100

    adding imports

--- runpytorchmodel_26072022.py
+++ runpytorchmodel_26072022.py
@@ -100,113 +100,26 @@ from YoutubeCodeRepository.ReinforcementLearning.DeepQLearning.utils import plot
 import numpy as np
 
 
-class Agent:
-    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions,
-                 max_mem_size=100000, eps_end=0.05, eps_dec=5e-4):
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.eps_min = eps_end
-        self.eps_dec = eps_dec
-        self.lr = lr
-        self.action_space = [i for i in range(n_actions)]
-        self.mem_size = max_mem_size
-        self.batch_size = batch_size
-        self.mem_cntr = 0
-        self.iter_cntr = 0
-        self.replace_target = 100
-
-        self.Q_eval = simple_dqn_torch_2020.DeepQNetwork(lr, n_actions=n_actions,
-                                                         input_dims=input_dims,
-                                                         fc1_dims=256, fc2_dims=256)
-        self.state_memory = np.zeros((self.mem_size, *input_dims),
-                                     dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_dims),
-                                         dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
-        self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
-
-    def store_transition(self, state, action, reward, state_, terminal):
-        index = self.mem_cntr % self.mem_size
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.reward_memory[index] = reward
-        self.action_memory[index] = action
-        self.terminal_memory[index] = terminal
-
-        self.mem_cntr += 1
-
-    def choose_action(self, observation):
-        if np.random.random() > self.epsilon:
-            state = T.tensor([observation]).to(self.Q_eval.device)
-            actions = self.Q_eval.forward(state)
-            action = T.argmax(actions).item()
-        else:
-            action = np.random.choice(self.action_space)
-
-        return action
-
-    def learn(self):
-        # print('calling machine learning phil''s function')
-        if self.mem_cntr < self.batch_size:
-            return
-
-        self.Q_eval.optimizer.zero_grad()
-
-        max_mem = min(self.mem_cntr, self.mem_size)
-
-        batch = np.random.choice(max_mem, self.batch_size, replace=False)
-        batch_index = np.arange(self.batch_size, dtype=np.int32)
-
-        state_batch = T.tensor(self.state_memory[batch]).to(self.Q_eval.device)
-        new_state_batch = T.tensor(
-            self.new_state_memory[batch]).to(self.Q_eval.device)
-        action_batch = self.action_memory[batch]
-        reward_batch = T.tensor(
-            self.reward_memory[batch]).to(self.Q_eval.device)
-        terminal_batch = T.tensor(
-            self.terminal_memory[batch]).to(self.Q_eval.device)
-
-        q_eval = self.Q_eval.forward(state_batch)[batch_index, action_batch]
-        q_next = self.Q_eval.forward(new_state_batch)
-        q_next[terminal_batch] = 0.0
-
-        q_target = reward_batch + self.gamma * T.max(q_next, dim=1)[0]
-
-        loss = self.Q_eval.loss(q_target, q_eval).to(self.Q_eval.device)
-        loss.backward()
-        self.Q_eval.optimizer.step()
-
-        self.iter_cntr += 1
-        self.epsilon = self.epsilon - self.eps_dec \
-            if self.epsilon > self.eps_min else self.eps_min
+def obs_to_torch(obs):
+    # TF: NHWC
+    # PyTorch: NCHW
+    # https://discuss.pytorch.org/t/dimensions-of-an-input-image/19439
+    # obs = np.transpose(obs, (0, 3, 1, 2))
+    # # Normalize
+    # obs = obs / 255.0
+    obs = th.tensor(obs).float()
+    return obs
 
 
-agent = Agent(gamma=0.99, epsilon=1.0, batch_size=64, n_actions=4, eps_end=0.01,
-              input_dims=[9], lr=0.001)
-scores, eps_history = [], []
-n_games = 500
+env = gym.make('LunarLander-v4')
 
-for i in range(n_games):
-    score = 0
-    done = False
-    observation = env.reset()
-    while not done:
-        action = agent.choose_action(observation)
-        observation_, reward, done, info = env.step(action)
-        score += reward
-        agent.store_transition(observation, action, reward,
-                               observation_, done)
-        agent.learn()
-        observation = observation_
-    scores.append(score)
-    eps_history.append(agent.epsilon)
+episode_reward = 0
+done = False
+obs = env.reset()
+while not done:
+    action = th.argmax(dqn_torch_v(obs_to_torch(obs)).cuda()).item()
+    # action = env.action_space.sample()
+    obs, reward, done, _ = env.step([action])
+    episode_reward += reward
 
-    avg_score = np.mean(scores[-100:])
-
-    print('episode ', i, 'score %.2f' % score,
-          'average score %.2f' % avg_score,
-          'epsilon %.2f' % agent.epsilon)
-x = [i + 1 for i in range(n_games)]
-filename = 'lunar_lander.png'
-plotLearning(x, scores, eps_history, filename)
+print(episode_reward)
