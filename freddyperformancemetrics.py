9d0af8e6b79b66b74860cf0634679084b2bf293b
carlacodes <carla.griffiths@gmail.com>
2022-07-27 23:37:57 +0100

    modifying plotting functions

--- freddyperformancemetrics.py
+++ freddyperformancemetrics.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 
 def jumpstart(y):
@@ -22,29 +23,31 @@ def transfer_ratio(y_transfer, y_reference, total_reward):
     return transfer_ratio
 
 
-def time_to_threshold(y, threshold=275):
-    return np.argmax(y > threshold)
+def time_to_threshold(x, y, threshold=250):
+    return x[np.argmax(y > threshold)]
 
 
 def max_reward_fnc(y):
     return np.max(y), np.argmax(y)
 
 
-def performance_metrics(y, rolling_avg=10, threshold=275, plot=True):
+def performance_metrics(x, y, rolling_avg=10, threshold=275, plot=True):
     '''
     TODO: write plots functions
     '''
 
     max_reward, idx_training_max = max_reward_fnc(y)
     total_reward = total_reward_fnc(y)
-    n_timesteps = len(y)
+    n_timesteps = x.max()
     reward_per_timestep = total_reward / n_timesteps
     asymptote = asymptotic_performance(y, rolling_avg=rolling_avg)
     std_asymptote = np.std(y[-rolling_avg:])
     asymptote_after_max = asymptotic_performance(y, rolling_avg=n_timesteps - idx_training_max)
     std_asymptote_after_max = np.std(y[-(n_timesteps - idx_training_max):])
-    time_threshold = time_to_threshold(y, threshold=threshold)
-    time_threshold_80_of_max = time_to_threshold(y, threshold=0.80 * max_reward)
+    time_threshold = time_to_threshold(x, y, threshold=threshold)
+    threshold_80 = 0.80 * threshold
+    time_threshold_80_of_max = time_to_threshold(x, y, threshold=threshold_80)
+    print(0.80 * max_reward)
 
     print(f" Max reward: {max_reward} \
         \n Time steps to max reward: {idx_training_max}/{n_timesteps} ({idx_training_max * 100 / n_timesteps:.2f}% of training period) \
@@ -53,16 +56,21 @@ def performance_metrics(y, rolling_avg=10, threshold=275, plot=True):
         \n Asymptotic performance (last {rolling_avg} timesteps): {asymptote:.2f} ±{std_asymptote:.2f} ({asymptote * 100 / max_reward:.2f}% of max reward) \
         \n Asymptotic performance after max: {asymptote_after_max:.2f} ±{std_asymptote_after_max:.2f} ({asymptote_after_max * 100 / max_reward:.2f}% of max reward) \
         \n Time to threshold(={threshold}): {time_threshold}/{n_timesteps} ({time_threshold * 100 / n_timesteps:.2f}% of training period) \
-        \n Time to threshold(80% of max): {time_threshold_80_of_max}/{n_timesteps} ({time_threshold_80_of_max * 100 / n_timesteps:.2f}% of training period)" \
+        \n Time to threshold(80% of max={threshold_80}): {time_threshold_80_of_max}/{n_timesteps} ({time_threshold_80_of_max * 100 / n_timesteps:.2f}% of training period)" \
           )
 
     if plot == True:
-        sns.scatterplot(idx_training_max, max_reward)
-        plt.title('Reward Over Timesteps')
+        sns.scatterplot(x=x, y=y, color='green')
+        plt.title('Reward Over Timesteps 2')
+        plt.xlabel('Episode Index')
+        plt.ylabel('Reward')
         plt.show()
 
-        sns.barplot(data=[asymptote])
+        # sns.barplot(data=[asymptote])
+        # plt.show()
+        numpy_data = ([threshold, time_threshold_80_of_max])
 
-        sns.barplot(data=[threshold, time_threshold_80_of_max])
+        plt.bar([0, 1], [time_threshold, time_threshold_80_of_max])
+        plt.xticks([0, 1], labels=['Time to Max Threshold', 'Time to 80% max threshold'])
         plt.title('Threshold Time')
         plt.show()
